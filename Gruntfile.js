6fe078832914ea8235bcd131aad9e8672e72d063
Burke <burkegreen@gmail.com>
2018-08-01 12:17:32 -0700

    Proxy working for all components.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,40 +4,23 @@ module.exports = (grunt) => {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
-    exec: {
-      install_deps: {
-        cwd: 'appointments',
-        cmd: 'npm install',
-        notify: 'echo "npm install complete?"',
-      },
-      other_deps: {
-        cwd: 'reviews',
-        cmd: 'npm install',
-        notify: 'isntalled npm deps for reviews',
-      },
-    },
     concat: {
       options: {
         separator: ';',
       },
       dist: {
-        
         src: [
-        'reviews/public/dist/bundle.js',
-        'appointments/public/dist/bundle.js',
         'image-carousel/client/dist/bundle.js',
+        'reviews/public/dist/bundle.js',
         'maps_info/client/dist/bundle.js',
+        'appointments/public/dist/bundle.js',
         ],
 
         dest: 'proxy/public/bundle.js',
       },
     },
-
   });
-  grunt.loadNpmTasks('grunt-express-server');
-  grunt.loadNpmTasks('grunt-exec');
+
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-run');
   grunt.registerTask('default', 'this is the help answer!', ['concat', /*'run', 'exec', 'express',*/ ]);
 };
---
+++ old_proxy/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+//const request = require('request');
+const bodyParser = require('body-parser');
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(function (request, res, next) {
+  console.log(request.method, request.path);
+  next();
+});
+
+// let getReviewsByNumber = function(id, callback) {
+//   console.log('inside getReviewsByNumber');
+//   let myURL = `/locations/${locID}/reviews`;
+//   let options = {
+//     url: myURL,
+//     headers: {
+//       contentType: 'application/json',
+//     },
+//   }
+//   request.get(options, callback);
+// }
+
+
+
+app.use('/:id/', express.static(path.join(__dirname, '/public')));
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
