5c143f42cf6c297de7257f74d61a172f6916bf39
Burke <burkegreen@gmail.com>
2018-08-05 20:38:04 -0700

    Removed extraneous files.

--- server.js
+++ server.js
@@ -1,43 +1,22 @@
-const express = require('express');
-//const morgan = require('morgan');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 3000;
-//const request = require('request');
-const bodyParser = require('body-parser');
 
-//app.use(morgan('dev'));
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const PORT = 3000;
+const app = express();
 
 app.use(bodyParser.json());
-
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-app.use(function (request, res, next) {
-  console.log(request.method, request.path);
-  next();
-});
-
-// let getReviewsByNumber = function(id, callback) {
-//   console.log('inside getReviewsByNumber');
-//   let myURL = `/locations/${locID}/reviews`;
-//   let options = {
-//     url: myURL,
-//     headers: {
-//       contentType: 'application/json',
-//     },
-//   }
-//   request.get(options, callback);
-// }
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/:businessId/', express.static(path.join(__dirname, './public')));
 
 
 
-app.use('/:id/', express.static(path.join(__dirname, '/public')));
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 
-app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
-});
+module.exports = app;
